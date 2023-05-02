abc2745f0aaefe88455c4e5078bb7ca2add49810
arman1549 <armanhossen1549@gmail.com>
2023-05-02 11:55:12 +0600

    recipes json

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express =require('express')
 const app =express()
 const port = process.env.PORT || 5000;
 const chefs =require('./data/chefs.json')
+const recipes =require('./data/Recipes.json')
 
 
 app.get('/',(req, res)=>{
