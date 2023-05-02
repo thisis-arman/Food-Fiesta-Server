3223b8a7f1739350ed10739edaf82ddbd4225a57
arman1549 <armanhossen1549@gmail.com>
2023-05-02 11:53:41 +0600

    chefs json uploaded

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express =require('express')
 const app =express()
 const port = process.env.PORT || 5000;
+const chefs =require('./data/chefs.json')
 
 
 app.get('/',(req, res)=>{
     res.send('Food Fiesta Server')
 
+})
+app.get('/chefs',(req,res)=>{
+    res.send(chefs)
 })
 
 app.listen(port,()=>{
